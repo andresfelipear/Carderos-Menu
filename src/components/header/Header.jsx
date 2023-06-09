d2db870738520485318d1f659dbb5bdccbfdaf17
Andres Arevalo <andresfelipear@gmail.com>
2023-06-08 23:22:45 -0700

    working in the header.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className="bg-gray-900 text-zinc-300 block">
-      <div className="py-4 text-center font-medium w-fit m-auto border-b-2 mb-10">
-        <h1 className="uppercase text-7xl">design lux</h1>
-        <p className="text-right italic">We design what you dream</p>
-      </div>
-      <nav className="grid grid-cols-6 gap-4 p-4 border-t-8 text-sm">
-        <div className='flex capitalize col-start-2 col-span-4 justify-around'>
-          <a href="#">services</a>
-          <a href="#">interior design</a>
-          <a href="#">home automation</a>
+    <header className="bg-secondary text-zinc-300 block">
+      <div className="py-4 text-center font-medium  m-auto flex items-center grid grid-cols-3 gap-4">
+        <div className='col-start-2 w-fit'>
+          <h1 className="text-primary text-5xl tracking-widest">cardero's</h1>
+          <div className="uppercase tracking-[.50em] italic text-gray-400 text-[10px] text-right">restaurant</div>
         </div>
-        <div className='text-right space-x-4'>
-          <a href="#">Login</a>
-          <a href="#" className='bg-zinc-300 text-gray-900 py-2 px-1.5 rounded-lg font-medium'>Sign Up</a>
+        <div className="text-gray-600">
+          Tel: <strong>604.669.7666</strong>
+        </div>
+
+      </div>
+      <nav className="grid grid-cols-6 gap-4 p-4 text-sm text-basic bg-gray-900">
+        <div className='flex capitalize col-start-2 col-span-4 justify-around'>
+          <a href="#">reserve online</a>
+          <a href="#">menus</a>
+          <a href="#">contact & location</a>
+          <a href="#">private events</a>
         </div>
       </nav>
     </header>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#d49f59',
+        secondary: '#f0f0f1',
+        basic: '#fff'
+      },
+    },
+  },
+  plugins: [],
+}
