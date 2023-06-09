a0ef78ed528d2616f2b3db1fdb9764a529437742
Andres Arevalo <andresfelipear@gmail.com>
2023-06-08 21:50:28 -0700

    defining basic structure. Installing imp packs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom"
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 function App() {
+  const queryCliente = new QueryClient()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryCliente}>
+      <div className="App font-sans">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+        <Footer />
+      </div>
+
+    </QueryClientProvider>
+
   );
 }
 
