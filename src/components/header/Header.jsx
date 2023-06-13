0019a7eb0b6044112bf3084ea1cfe0844dfa026d
Andres Arevalo <andresfelipear@gmail.com>
2023-06-13 05:49:11 -0700

    centering title

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="bg-secondary block">
       <div className="py-4 text-center font-medium  m-auto flex justify-center items-center grid md:grid-cols-3  md:gap-4">
-        <div className='md:col-start-2 w-fit'>
+        <div className='md:col-start-2 w-fit mx-auto'>
           <h1 className="text-primary text-3xl md:text-5xl tracking-widest">cardero's</h1>
           <div className="uppercase tracking-[.50em] italic text-gray-400 text-[0.50rem] text-right">restaurant</div>
         </div>
